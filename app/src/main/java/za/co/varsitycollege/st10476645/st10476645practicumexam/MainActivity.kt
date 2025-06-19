fbb6e89c5f3b25a9dc0c84526a923a3cfbc7a708
st10476645 <alexejemimar@gmail.com>
2025-06-19 09:22:04 +0200

    Initial commit

---
+++ app/src/main/java/za/co/varsitycollege/st10476645/st10476645practicumexam/MainActivity.kt
@@ -0,0 +1,34 @@
+package za.co.varsitycollege.st10476645.st10476645practicumexam
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val songtitle = arrayListOf<String>(R.id.songtitle.toString())
+        val artistname = arrayListOf<String>(R.id.artistname.toString())
+        val rating = arrayListOf<String>(R.id.rating.toString())
+        val comments = arrayListOf<String>(R.id.comments.toString())
+
+
+        val addtoplaylistbtn = findViewById<Button>(R.id.addtoplaylistbtn)
+        val exitbtn = findViewById<Button>(R.id.exitbtn)
+
+
+
+    }
+}
